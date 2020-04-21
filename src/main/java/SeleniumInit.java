0fcb1fdce6561fc5517982d1eb174009780677fb
Francisco Moreno <francisco.moreno@sngular.com>
2020-04-21 19:59:09 +0200

    changes

--- src/main/java/SeleniumInit.java
+++ src/main/java/SeleniumInit.java
@@ -1,14 +1,23 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumInit {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://duckduckgo.com/");
+
+        driver.findElement(By.id("search_form_input_homepage")).sendKeys("Naranja");
+
+        driver.findElement(By.id("search_button_homepage")).click();
+
+        Thread.sleep(5000);
+
+        driver.quit();
     }
 }
