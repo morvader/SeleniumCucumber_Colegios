4cf13ccc80babd0f65cd2b189d69c7dab21fc8b9
Francisco Moreno <francisco.moreno@sngular.com>
2020-04-28 19:06:43 +0200

    Refactor

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,11 +1,18 @@
 package Tests;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.TheInternet.MainPage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     public WebDriver driver;
@@ -22,8 +29,15 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result) throws IOException {
+        if(result.getStatus() == ITestResult.FAILURE)
+            takeScreenshot(driver, "./screenshots", result.getName());
         driver.quit();
+    }
+
+    public static void takeScreenshot(WebDriver driver, String destinationPath, String testName) throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File(destinationPath, testName + ".png"));
     }
 
 }
