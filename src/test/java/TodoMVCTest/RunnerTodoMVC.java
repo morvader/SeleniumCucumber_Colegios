23fa80b210f814ff97041155c29f484c820cf01d
Francisco Moreno <francisco.moreno@sngular.com>
2020-04-29 19:13:57 +0200

    cucumber

---
+++ src/test/java/TodoMVCTest/RunnerTodoMVC.java
@@ -0,0 +1,17 @@
+package TodoMVCTest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features = "src/test/resources/features/TodoMVC",
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber",
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"
+        },
+        glue = {"TodoMVCTest"}
+)
+public class RunnerTodoMVC extends AbstractTestNGCucumberTests {
+}
